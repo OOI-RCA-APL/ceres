23d0349f93f18f81b30131cd21d07eeeb50f067e
Jake Ploskey <jploskey@uw.edu>
2023-01-18 16:53:03 -0800

    Use multiple database connections again

--- ceres/ceres/database/adapters/postgres.py
+++ ceres/ceres/database/adapters/postgres.py
@@ -1,5 +1,7 @@
 from typing import Any, final
 
+from sqlalchemy import QueuePool
+
 from ...config import PostgresDatabaseConfig
 from ..adapter import DatabaseAdapter
 
@@ -15,6 +17,9 @@ class PostgresDatabaseAdapter(DatabaseAdapter[PostgresDatabaseConfig]):
 
     def get_engine_config(self) -> dict[str, Any]:
         return {
+            "poolclass": QueuePool,
+            "pool_size": 10,
+            "max_overflow": -1,
             "pool_pre_ping": True,  # Check to see if a connection has closed before use.
             "pool_recycle": 60 * 5,  # Drop unused connections after 5 minutes.
             **self.config.engine,
