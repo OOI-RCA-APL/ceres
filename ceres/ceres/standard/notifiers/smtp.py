fb458ac0a940e39f467e3041fb1af5922e4ad28d
Jake Ploskey <jploskey@uw.edu>
2023-04-13 15:20:54 -0700

    Separate username and sender in SMTPNotifier

--- ceres/ceres/standard/notifiers/smtp.py
+++ ceres/ceres/standard/notifiers/smtp.py
@@ -9,11 +9,11 @@ from ceres.roles.notifier import Notification, Notifier
 
 
 class SMTPNotifier(Notifier):
-    email: str
-    username: str | None = None
-    password: SecretStr | None = None
     host: str
     port: int = Field(ge=0)
+    sender: str | None = None
+    username: str | None = None
+    password: SecretStr | None = None
     use_tls: bool = False
     use_starttls: bool = False
 
@@ -24,16 +24,12 @@ class SMTPNotifier(Notifier):
         recipients: Sequence[str],
     ) -> None:
         message = EmailMessage()
-        message["From"] = self.email
+        if self.sender is not None:
+            message["From"] = self.sender
         message["To"] = ",".join(recipient.strip() for recipient in recipients)
         message["Subject"] = notification.subject
         message.set_type(notification.content_type)
         message.set_payload(notification.content, charset="utf-8")
-
-        if self.username is not None:
-            username = self.username
-        else:
-            username = self.email
 
         if self.password is not None:
             password = self.password.get_secret_value()
@@ -44,7 +40,7 @@ class SMTPNotifier(Notifier):
             message=message,
             hostname=self.host,
             port=self.port,
-            username=username,
+            username=self.username,
             password=password,
             use_tls=self.use_tls,
             start_tls=self.use_starttls,
