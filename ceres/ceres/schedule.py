be8c853b6c46346e5975a913fb4a845a7ce62d25
Jake Ploskey <jploskey@uw.edu>
2023-05-05 10:32:16 -0700

    Calculate interval scheduler with multiplier performantly

--- ceres/ceres/schedule.py
+++ ceres/ceres/schedule.py
@@ -1,16 +1,16 @@
 import datetime as dt
+import math
 from abc import abstractmethod
 from datetime import datetime, timedelta
 from enum import Enum
-from typing import TYPE_CHECKING, Iterable, Literal, Sequence
+from typing import TYPE_CHECKING, Any, Iterable, Literal, Mapping, Sequence
 
 from apscheduler.triggers.cron import CronTrigger as InternalCronTrigger
 from apscheduler.triggers.interval import IntervalTrigger as BaseInternalIntervalTrigger
 from apscheduler.util import normalize
-from pydantic import validator
+from pydantic import PositiveFloat, validator
 
 from ceres.data import DateTime, ImmutableDataObject, PositiveTimeDelta
-from ceres.internal.utilities import CacheDict
 
 
 class ScheduleKind(str, Enum):
@@ -52,9 +52,9 @@ class IntervalSchedule(BaseSchedule):
     interval: PositiveTimeDelta
     start: DateTime | None = None
     end: DateTime | None = None
-    multiplier: float = 1
-    min: timedelta | None = None
-    max: timedelta | None = None
+    multiplier: PositiveFloat = 1
+    min: PositiveTimeDelta | None = None
+    max: PositiveTimeDelta | None = None
 
     @validator("interval")
     def _validate_interval(cls, value: timedelta) -> timedelta:
@@ -62,6 +62,26 @@ class IntervalSchedule(BaseSchedule):
             raise ValueError("sub-second interval resolution is not allowed")
 
         return value
+
+    @validator("min")
+    def _validate_min(cls, min: float | None, values: Mapping[str, Any]) -> float | None:
+        interval = values.get("interval")
+        if min is None or interval is None:
+            return None
+        if min > interval:
+            raise ValueError("min must be <= interval")
+
+        return min
+
+    @validator("max")
+    def _validate_max(cls, max: float | None, values: Mapping[str, Any]) -> float | None:
+        interval = values.get("interval")
+        if max is None or interval is None:
+            return None
+        if max < interval:
+            raise ValueError("max must be >= interval")
+
+        return max
 
     def as_trigger(self) -> "IntervalTrigger":
         return IntervalTrigger(self)
@@ -125,91 +145,6 @@ class CronTrigger(Trigger):
         return self.__inner.get_next_fire_time(previous, now)
 
 
-class InternalIntervalTrigger(BaseInternalIntervalTrigger):
-    if TYPE_CHECKING:
-        start_date: datetime
-
-    def __init__(
-        self,
-        weeks: int = 0,
-        days: int = 0,
-        hours: int = 0,
-        minutes: int = 0,
-        seconds: int = 0,
-        start_date: datetime | None = None,
-        end_date: datetime | None = None,
-        jitter: float | None = None,
-        multiplier: float = 1,
-        min: timedelta | None = None,
-        max: timedelta | None = None,
-    ):
-        super().__init__(
-            weeks=weeks,
-            days=days,
-            hours=hours,
-            minutes=minutes,
-            seconds=seconds,
-            start_date=start_date,
-            end_date=end_date,
-            timezone=dt.timezone.utc,
-            jitter=jitter,
-        )
-        self.multiplier = multiplier
-        self.min = min
-        self.max = max
-        self.__cache: CacheDict[datetime, int] = CacheDict()
-
-    def get_next_fire_time(
-        self,
-        previous_fire_time: datetime | None,
-        now: datetime,
-    ) -> datetime | None:
-        if self.end_date is not None and now > self.end_date:
-            return None
-
-        next_fire_time: datetime | None = None
-
-        if now < self.start_date:
-            next_fire_time = self.start_date
-        else:
-            start = self.start_date.timestamp()
-            end = now.timestamp()
-            base = self.interval.total_seconds()
-            min = self.min.total_seconds() if self.min is not None else None
-            max = self.max.total_seconds() if self.max is not None else None
-
-            iteration = 0
-            current = start
-            for previous_now, previous_iteration in self.__cache.items():
-                if now >= previous_now and previous_iteration > iteration:
-                    iteration = previous_iteration
-
-            while current < end:
-                interval = base * self.multiplier**iteration
-                if min is not None and interval < min:
-                    interval = min
-                if max is not None and interval > max:
-                    interval = max
-
-                if current + interval >= end:
-                    current = current + interval
-                    self.__cache[now] = iteration
-                    break
-
-                current += interval
-                iteration += 1
-
-            next_fire_time = datetime.fromtimestamp(current, dt.timezone.utc)
-
-        if self.jitter is not None:
-            next_fire_time = self._apply_jitter(next_fire_time, self.jitter, now)
-
-        if self.end_date is not None and next_fire_time > self.end_date:
-            return None
-
-        return normalize(next_fire_time)
-
-
 class IntervalTrigger(Trigger):
     def __init__(self, schedule: IntervalSchedule) -> None:
         super().__init__()
@@ -251,3 +186,183 @@ class OrTrigger(Trigger):
                 minimum = current
 
         return minimum
+
+
+class InternalIntervalTrigger(BaseInternalIntervalTrigger):
+    if TYPE_CHECKING:
+        start_date: datetime
+
+    def __init__(
+        self,
+        weeks: int = 0,
+        days: int = 0,
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: int = 0,
+        start_date: datetime | None = None,
+        end_date: datetime | None = None,
+        jitter: float | None = None,
+        multiplier: float = 1,
+        min: timedelta | None = None,
+        max: timedelta | None = None,
+    ):
+        super().__init__(
+            weeks=weeks,
+            days=days,
+            hours=hours,
+            minutes=minutes,
+            seconds=seconds,
+            start_date=start_date,
+            end_date=end_date,
+            timezone=dt.timezone.utc,
+            jitter=jitter,
+        )
+        self.multiplier = multiplier
+        self.min = min
+        self.max = max
+
+    def get_next_fire_time(
+        self,
+        previous_fire_time: datetime | None,
+        now: datetime,
+    ) -> datetime | None:
+        if self.end_date is not None and now > self.end_date:
+            return None
+
+        next_fire_time: datetime | None = None
+
+        if now < self.start_date:
+            next_fire_time = self.start_date
+        else:
+            runtime = now - self.start_date
+            delay = _compute_fire_time_delay(
+                runtime=runtime,
+                interval=self.interval,
+                multiplier=self.multiplier,
+                min=self.min,
+                max=self.max,
+            )
+            if delay is None:
+                return None
+
+            next_fire_time = self.start_date + delay
+
+        if self.jitter is not None:
+            next_fire_time = self._apply_jitter(next_fire_time, self.jitter, now)
+
+        if self.end_date is not None and next_fire_time > self.end_date:
+            return None
+
+        return normalize(next_fire_time)
+
+
+def _compute_runtime(
+    interval: timedelta,
+    multiplier: float,
+    iterations: int,
+) -> timedelta:
+    if multiplier == 1:
+        return interval * iterations
+
+    # https://www.wolframalpha.com/input?i2d=true&i=simplify+d+%3DSum%5Bv*Power%5Bm%2Ck%5D%2C%7Bk%2C0%2Cn+-+1%7D%5D
+    return (interval * ((multiplier**iterations) - 1)) / (multiplier - 1)
+
+
+# def _compute_runtime_slow(
+#     interval: timedelta,
+#     multiplier: float,
+#     iterations: int,
+# ) -> Any:
+#     current = timedelta()
+#     k = 0
+#     while k < iterations:
+#         current += interval * (multiplier**k)
+#         k += 1
+#     return current
+
+
+def _compute_fire_time_delay(
+    *,
+    runtime: timedelta,
+    interval: timedelta,
+    multiplier: float,
+    min: timedelta | None = None,
+    max: timedelta | None = None,
+) -> timedelta | None:
+    result = _compute_iterations_and_fire_time_delay(
+        runtime=runtime,
+        interval=interval,
+        multiplier=multiplier,
+        min=min,
+        max=max,
+    )
+
+    if result is None:
+        return None
+
+    _, next_fire_time = result
+    return next_fire_time
+
+
+def _compute_iterations_and_fire_time_delay(
+    *,
+    runtime: timedelta,
+    interval: timedelta,
+    multiplier: float,
+    min: timedelta | None = None,
+    max: timedelta | None = None,
+) -> tuple[int, timedelta] | None:  # iterations, next_fire_time
+    if runtime <= interval:
+        return 1, interval
+    if multiplier < 1:
+        limit = min
+    elif multiplier > 1:
+        limit = max
+    else:
+        iterations = math.ceil(runtime / interval)
+        return iterations, interval * iterations
+
+    if limit is None:
+        # Compute the number of iterations before the desired runtime is reached.
+        # https://www.symbolab.com/solver/step-by-step/solve%20for%20n%2C%20d%20%3D%20%5Cleft(v%20%5Cleft(m%5E%7B%5Cleft(n%20%2B%201%5Cright)%7D%20-%201%5Cright)%5Cright)%2F%5Cleft(m%20-%201%5Cright)?or=input
+        try:
+            iterations = math.ceil(
+                (math.log(((runtime * (multiplier - 1)) / interval) + 1) / math.log(multiplier))
+            )
+        except ValueError:
+            return None
+
+        return iterations, _compute_runtime(interval, multiplier, iterations)
+
+    # Compute the number of iterations before the min/max limit is reached.
+    # https://www.symbolab.com/solver/step-by-step/solve%20for%20k%2C%20t%3Dv%20%5Ccdot%20m%5E%7Bk%7D?or=input
+    try:
+        pre_limit_iterations = math.ceil(math.log(limit / interval) / math.log(multiplier)) + 1
+    except ValueError:
+        return None
+
+    pre_limit_runtime = _compute_runtime(interval, multiplier, pre_limit_iterations)
+
+    # If the min/max limit is not reached before the desired runtime, then just run the function
+    # again without it.
+    if runtime < pre_limit_runtime:
+        return _compute_iterations_and_fire_time_delay(
+            runtime=runtime,
+            interval=interval,
+            multiplier=multiplier,
+        )
+
+    remaining_runtime = runtime - pre_limit_runtime
+    post_limit_result = _compute_iterations_and_fire_time_delay(
+        runtime=remaining_runtime,
+        interval=limit,
+        multiplier=1,
+    )
+    if post_limit_result is None:
+        return None
+
+    post_limit_iterations, post_limit_runtime = post_limit_result
+    iterations = pre_limit_iterations + post_limit_iterations - 1
+    next_fire_time_delay = pre_limit_runtime + post_limit_runtime
+
+    return iterations, next_fire_time_delay
